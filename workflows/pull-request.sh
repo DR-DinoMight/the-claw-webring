07a347487680a737ebbebadb3c1716edec6a5dbd
Matthew Peck-Deloughry <matt@deloughry.co.uk>
2022-12-15 12:21:35 +0000

    tests

--- workflows/pull-request.sh
+++ workflows/pull-request.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-git fetch origin main
-# Run the `git diff` command to compare the current branch with the `origin/main` branch
-URLS=$(git diff origin/main..HEAD --unified=0 -- ./data/members.json  | grep '^+' | grep -o '\"url\":\s\".*\"' | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u)
-
+# URLS=$(git diff origin/main..HEAD --unified=0 -- ./data/members.json  | grep '^+' | grep -o '\"url\":\s\".*\"' | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u)
 # Use a for loop to iterate over the URLs in the $URLS variable
+
+# Check if URLs is not empty if so exit 0
+if [ -z "$URLS" ]; then
+	exit 0
+fi
+
 for url in $URLS; do
   # Use curl and grep to search for the <the-claw-webring-widget> tag in the URL
   curl -s $url | grep -qo "<the-claw-webring-widget>"
