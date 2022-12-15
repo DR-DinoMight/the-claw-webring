494729e944b6cc6b5d31d5f07d99bfbde76f8994
Matthew Peck-Deloughry <matt@deloughry.co.uk>
2022-12-15 08:30:03 +0000

    Added check before PR to see if Webring is present

--- workflows/pull-request.sh
+++ workflows/pull-request.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 git fetch origin main
 # Run the `git diff` command to compare the current branch with the `origin/main` branch
-URLS=$(git diff origin/main..HEAD --unified=0 -- ./data/members.json | grep '"url"'  | grep -o '\"url\":\s\".*\"' | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u)
+URLS=$(git diff origin/main..HEAD --unified=0 -- ./data/members.json  | grep '^+' | grep -o '\"url\":\s\".*\"' | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u)
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
 # Use a for loop to iterate over the URLs in the $URLS variable
 for url in $URLS; do
-	echo "Looking at $url"
   # Use curl and grep to search for the <the-claw-webring-widget> tag in the URL
   curl -s $url | grep -qo "<the-claw-webring-widget>"
 
   # Check the exit status of the previous command
   if [ $? -eq 0 ]; then
     # Print a message indicating that the tag was found in the URL
-    echo "The <the-claw-webring-widget> tag was found in $url"
+    echo "${GREEN}$url: ✅${NC}"
   else
     # Print a message indicating that the tag was not found in the URL
-    echo "The <the-claw-webring-widget> tag was not found in $url"
-    exit -1
+    echo "${RED}$url: ❌${NC}"
+    exit 1
   fi
 done
 
