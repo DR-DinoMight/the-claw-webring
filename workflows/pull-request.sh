7955306eae59ca9f23e383be7c14d759450eb041
Matthew Peck-Deloughry <matt@deloughry.co.uk>
2022-12-15 08:20:46 +0000

    added workflow and test data

---
+++ workflows/pull-request.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+git fetch origin main
+# Run the `git diff` command to compare the current branch with the `origin/main` branch
+URLS=$(git diff origin/main..HEAD --unified=0 -- ./data/members.json | grep '"url"'  | grep -o '\"url\":\s\".*\"' | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u)
+
+# Use a for loop to iterate over the URLs in the $URLS variable
+for url in $URLS; do
+	echo "Looking at $url"
+  # Use curl and grep to search for the <the-claw-webring-widget> tag in the URL
+  curl -s $url | grep -qo "<the-claw-webring-widget>"
+
+  # Check the exit status of the previous command
+  if [ $? -eq 0 ]; then
+    # Print a message indicating that the tag was found in the URL
+    echo "The <the-claw-webring-widget> tag was found in $url"
+  else
+    # Print a message indicating that the tag was not found in the URL
+    echo "The <the-claw-webring-widget> tag was not found in $url"
+    exit -1
+  fi
+done
+
+echo "Finished searching for and it passed"
+exit 0
